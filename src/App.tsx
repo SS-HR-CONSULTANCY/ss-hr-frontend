67df12f70e4e2954bf9656b446bf5d6d0b049c07
Tony Kuriakose <t4tonykuriakose@gmail.com>
2025-08-15 15:51:43 +0530

    frontend initialize

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+
+
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <div className="max-w-4xl mx-auto space-y-8">
+        <div className="card">
+          <h1 className="prata-regular text-4xl text-center text-red-900 mb-4">
+            SS HR Consultancy
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
