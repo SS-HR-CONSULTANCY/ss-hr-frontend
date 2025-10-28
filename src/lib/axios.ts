f301ccf997db76713b3426bf2c2cc732deb46290
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-10-28 19:38:23 +0530

    refactor : axios and background-beams updated

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,8 +5,6 @@ const baseURL =
     ? import.meta.env.VITE_BACKEND_DEVELOPMENT_URL
     : import.meta.env.VITE_BACKEND_PRODUCTION_URL;
 
-console.log("baseURL : ", baseURL);
-
 export const axiosInstance = axios.create({
   baseURL,
   withCredentials: true,
